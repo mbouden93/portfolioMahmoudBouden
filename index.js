49163d1aa1f7df9a46049ac8606dde6af98f8c5e
Mahmoud <mahmoud.bouden@esprit.tn>
2017-10-14 18:49:35 +0100

    m

---
+++ index.js
@@ -0,0 +1,23 @@
+﻿'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.sendFile("./index.html")
+})
