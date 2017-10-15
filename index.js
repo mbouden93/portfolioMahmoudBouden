dc8c62be727dfff5ef4cf6eccbcda38611925bb7
Mahmoud <mahmoud.bouden@esprit.tn>
2017-10-15 13:18:39 +0100

    message2

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
-app.use(express.static(__dirname + '/wp-includes'));
-app.use(express.static(__dirname + '/wp-content'));
-app.use(express.static(__dirname + '/wp-admin'));
+app.use(express.static(__dirname + '/wp-content/plugins/siteorigin-panels/css/frontd935.css?ver=2.4.24'));
+app.use(express.static(__dirname + '/wp-content/plugins/contact-form-7/includes/css/styles1c9b.css?ver=4.6.1'));
+app.use(express.static(__dirname + '/wp-content/themes/pulse/styleae9e.css?ver=4.7.6'));
+app.use(express.static(__dirname + '/wp-content/themes/pulse/css/bootstrap.minae9e.css?ver=4.7.6'));
+app.use(express.static(__dirname + '/wp-content/themes/pulse/css/font-awesome.minae9e.css?ver=4.7.6'));
+app.use(express.static(__dirname + '/wp-content/themes/pulse/css/normalizeae9e.css?ver=4.7.6'));
+app.use(express.static(__dirname + '/wp-content/themes/pulse/css/mainae9e.css?ver=4.7.6'));
+app.use(express.static(__dirname + '/wp-includes/js/jquery/jqueryb8ff.js?ver=1.12.4'));
+app.use(express.static(__dirname + '/wp-includes/js/jquery/jquery-migrate.min330a.js?ver=1.4.1'));
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
